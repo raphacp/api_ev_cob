caa664d20d22fdcf4f4344e0f064ad82eef8319c
Raphael <raphacp@gmail.com>
2022-04-29 16:26:00 -0300

    Versao inicial

---
+++ api_ev_cob.py
@@ -0,0 +1,6 @@
+# API que será responsável por marcar e dizer os vencedores das modalidades: 100m rasos e Lançamento de Dardo
+
+
+
+
+
---
+++ criacao_banco_api_ev_cob.sql
@@ -0,0 +1,57 @@
+-- Criação do banco de dados
+
+CREATE DATABASE api_ev_cob;
+
+USE api_ev_cob;
+
+-- Criação da tabela Atleta
+CREATE TABLE Atleta(
+Id INT NOT NULL AUTO_INCREMENT,
+Nome VARCHAR(150) NOT NULL,
+Pais VARCHAR(100) NOT NULL,
+Sexo ENUM("Masculino", "Feminino"),
+-- Paralimpico BOOL NOT NULL DEFAULT FALSE, # Fiquei na dúvida qual seria mais apropriado
+Paralimpico ENUM("Sim", "Não"),
+PRIMARY KEY (Id)
+);
+
+-- Criação da tabela Modalidade
+CREATE TABLE Modalidade(
+Id INT NOT NULL AUTO_INCREMENT,
+Nome VARCHAR(150) NOT NULL,
+PRIMARY KEY (Id)
+);
+
+-- Criação da tabela Prova
+CREATE TABLE Prova(
+Id INT NOT NULL AUTO_INCREMENT,
+Nome VARCHAR(150) NOT NULL,
+Unidade_Medida ENUM("s", "m"),
+Id_Modalidade INT NOT NULL,
+PRIMARY KEY (Id),
+FOREIGN KEY (Id_Modalidade) REFERENCES Modalidade(Id)
+);
+
+-- Criação da tabela Competicao
+CREATE TABLE Competicao(
+Id INT NOT NULL AUTO_INCREMENT,
+Nome VARCHAR(150) NOT NULL,
+Data_Inicio DATETIME NOT NULL,
+Data_Final DATETIME NOT NULL,
+Tipo ENUM("Masculino", "Feminino"),
+Paralimpico ENUM("Sim", "Não"),
+Id_Prova INT NOT NULL,
+PRIMARY KEY (Id),
+FOREIGN KEY (Id_Prova) REFERENCES Prova(Id)
+);
+
+-- Criação da tabela Competicao_Atleta
+CREATE TABLE Competicao_Atleta(
+Id_Competicao INT NOT NULL,
+Id_Atleta INT NOT NULL,
+Resultado FLOAT NOT NULL,
+PRIMARY KEY (Id_Competicao),
+PRIMARY KEY (Id_ATleta),
+FOREIGN KEY (Id_Competicao) REFERENCES Competicao(Id),
+FOREIGN KEY (Id_Atleta) REFERENCES Atleta(Id)
+);
